e07af34b59eed02c6bdddc1190c69d58e4b34ade
putra32 <putralubis1432@gmail.com>
2021-10-02 06:25:30 +0700

    Init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/putra32/app-say-hello.git
+
+go 1.17
+
+require github.com/putra32/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/putra32/go-say-hello"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello())
+}
