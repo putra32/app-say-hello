b97c97313b2036fb36d57ca42f66bcf590317b45
putra32 <putralubis1432@gmail.com>
2021-10-03 19:32:08 +0700

    change func

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	gosayhello "github.com/putra32/go-say-hello"
+	gosayhello "github.com/putra32/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(gosayhello.SayHello())
+	fmt.Println(gosayhello.SayHello("putra"))
 }
